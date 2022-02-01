41b76fe8d0a2c138190a4b051d143b1f7c0bc2b4
Miguelklappes <miguelklappes929@gmail.com>
2022-02-01 15:23:43 -0300

    Added gpu options

--- pkg/gpu/gpu.go
+++ pkg/gpu/gpu.go
@@ -31,8 +31,8 @@ type GraphicsCard struct {
 	Node                    *topology.Node `json:"node,omitempty"`
 	VideoModeDescription    string         `json:"video_mode_description"`
 	Caption                 string         `json:"caption"`
-	CreationClassName       string         `json:"creation_class_name"`
 	Description             string         `json:"description"`
+	CreationClassName       string         `json:"creation_class_name"`
 	DeviceID                string         `json:"device_id"`
 	Name                    string         `json:"name"`
 	PNPDeviceID             string         `json:"pnp_device_id"`
